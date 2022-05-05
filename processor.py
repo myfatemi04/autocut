f235f560cab049b876cfe9efa6272d6ece21e1c1
Michael Fatemi <myfatemi04@gmail.com>
2022-05-05 01:35:38 -0400

    fix minimum silence length

--- processor.py
+++ processor.py
@@ -21,47 +21,81 @@ def filter(video):
 
     active_clips = []
 
-    min_inactive_audio_cut = 0.05
+    min_inactive_audio_cut = 0.5
     min_active_audio_db = 10
 
     i = 0
     frame = 0
-    # if None, then the clip is not active. if not none, indicates when the activity began.
-    last_active_audio_start_frame = None
+    # If None, then the clip is not active. If not None, indicates when the activity began.
+    active_start_frame = None
+    # If not None, then we were active but then we suddenly dipped into inactivity.
+    # We use this because we don't want to cut out very short dips into inactivity.
+    inactive_start_frame = None
     for audio_chunk in tqdm.tqdm(video.audio.iter_chunks(chunk_duration=1/30), total=int(video.duration*30)):
         i += 1
         frame += len(audio_chunk)
         db = get_chunk_decibels(audio_chunk)
 
-        if last_active_audio_start_frame is not None:
+        if active_start_frame is not None:
             # Was active,
             if db < min_active_audio_db:
                 # ...but not loud enough now
-                start_time = last_active_audio_start_frame / video.audio.fps
-                end_time = frame / video.audio.fps
-                active_clips.append(video.subclip(start_time, end_time))
-                # Wait until min_inactive_audio_cut seconds have passed to reset last active audio start frame
-                if (frame - last_active_audio_start_frame) / video.audio.fps > min_inactive_audio_cut:
-                    last_active_audio_start_frame = None
 
-            # If it's currently active and we're above the dB threshold, don't do anything unusual
+                # We skim over patches of silence less than min_inactive_audio_cut seconds long.
+                if inactive_start_frame is None:
+                    # Mark the beginning of silence
+                    inactive_start_frame = frame
+
+                time_since_silence_begin = (
+                    frame - inactive_start_frame) / video.audio.fps
+
+                if time_since_silence_begin >= min_inactive_audio_cut:
+                    # Okay, cut it short now
+                    # Go from the beginning of the activity to the beginning of the silence
+                    start_time = active_start_frame / video.audio.fps
+                    end_time = inactive_start_frame / video.audio.fps
+                    active_clips.append(video.subclip(start_time, end_time))
+
+                    # Reset the state
+                    active_start_frame = None
+                    inactive_start_frame = None
+                else:
+                    # Don't cut it short yet; there might be active audio before min_inactive_audio_cut seconds are up
+                    pass
+            else:
+                # If it's currently active and we're above the dB threshold, don't do anything unusual
+                # Just make sure we don't think there's silence
+                if inactive_start_frame is not None:
+                    inactive_start_frame = None
         else:
             # Was inactive,
             if db > min_active_audio_db:
                 # ...but now it's loud enough
-                last_active_audio_start_frame = frame
+                active_start_frame = frame
 
+    # If we are currently active, add the last clip
+    if active_start_frame is not None:
+        start_time = active_start_frame / video.audio.fps
+        end_time = video.duration
+        active_clips.append(video.subclip(start_time, end_time))
+
+    # Concatenate the video clips
     # https://zulko.github.io/moviepy/getting_started/compositing.html
     composite = moviepy.editor.concatenate_videoclips(
         active_clips, method='compose')
 
-    # https://www.reddit.com/r/moviepy/comments/3uwuub/no_sound/
-    composite.write_videofile("IMG_4029_active.mp4", temp_audiofile="temp-audio.m4a",
-                              remove_temp=True, codec="libx264", audio_codec="aac")
-    composite.close()
+    return composite
 
-    video.close()
+
+def save_video(video):
+    # We need this workaround to get audio to render correctly
+    # https://www.reddit.com/r/moviepy/comments/3uwuub/no_sound/
+    video.write_videofile("IMG_4029_active.mp4", temp_audiofile="temp-audio.m4a",
+                          remove_temp=True, codec="libx264", audio_codec="aac")
 
 
 video = moviepy.editor.VideoFileClip('IMG_4029.MOV')
-filter(video)
+filtered = filter(video)
+save_video(filtered)
+filtered.close()
+video.close()
